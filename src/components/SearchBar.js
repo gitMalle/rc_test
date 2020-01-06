4549737733e30e8ec47808567077f0269f451fa3
Daniel Malesevic <daniel.malesevic@gmx.at>
2020-01-06 18:59:39 +0000

    Add search request to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../styles/SearchBar.css";
 
 export const SearchBar = props => {
   const [searchText, setSearchText] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    if (searchText.length > 1) {
+        fetchResults();
+    }
+  }, [searchText]);
+
+  const fetchResults = async () => {
+    const res = await axios.get(`https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&amp;solrRows=6&solrTerm=${searchText}`);
+    setResults(res.data.results.docs);
+    console.log(res.data.results.docs);
+  };
 
   return (
     <div>
