9155c0706530f80f91b89976e5b6569dd12111fe
Daniel Malesevic <daniel.malesevic@gmx.at>
2020-01-06 21:29:58 +0000

    Add form around search input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,27 +9,31 @@ export const SearchBar = props => {
 
   useEffect(() => {
     if (searchText.length > 1) {
-        fetchResults();
+      fetchResults();
     } else {
-        setResults([]);
+      setResults([]);
     }
   }, [searchText]);
 
   const fetchResults = async () => {
-    const res = await axios.get(`https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&amp;solrRows=6&solrTerm=${searchText}`);
+    const res = await axios.get(
+      `https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&amp;solrRows=6&solrTerm=${searchText}`
+    );
     setResults(res.data.results.docs);
   };
 
   return (
     <div className="position-relative">
-      <input
-        type="text"
-        className="form-control search-bar"
-        placeholder="city, airport, station, region and district..."
-        value={searchText}
-        onChange={ev => setSearchText(ev.target.value)}
-        autoFocus
-      />
+      <form>
+        <input
+          type="search"
+          className="form-control search-bar"
+          placeholder="city, airport, station, region and district..."
+          value={searchText}
+          onChange={ev => setSearchText(ev.target.value)}
+          autoFocus
+        />
+      </form>
       {results.length > 0 && <SearchResults results={results} />}
     </div>
   );
